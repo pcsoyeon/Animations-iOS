e390aede4669ae758de4d6e2d37369a5cc6a9a1e
pcsoyeon <alice9809@naver.com>
2021-06-15 19:15:03 +0900

    [Add] 폴더링 및 파일 추가

---
+++ NaviBarAnimation/NaviBarAnimation/Entry.swift
@@ -0,0 +1,24 @@
+//
+//  Entry.swift
+//  NaviBarAnimation
+//
+//  Created by soyeon on 2021/06/15.
+//
+
+import Foundation
+import UIKit
+
+class Entry {
+    
+    // MARK: - Properties
+    
+    let title: String
+    let image: UIImage
+    
+    // MARK: - Initializers
+    
+    init(title: String, image: UIImage) {
+        self.title = title
+        self.image = image
+    }
+}
--- NaviBarAnimation/NaviBarAnimation/ViewController.swift
+++ NaviBarAnimation/NaviBarAnimation/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var entries = [Entry(title: "First Entry", image: UIImage(named: "one")!),
+                   Entry(title: "Exploring", image: UIImage(named: "two")!),
+                   Entry(title: "Traveling Abroad", image: UIImage(named: "three")!),
+                   Entry(title: "Scuba Diving", image: UIImage(named: "four")!),
+                   Entry(title: "Trip Together", image: UIImage(named: "five")!),
+                   Entry(title: "The Unknown", image: UIImage(named: "six")!)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        entries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
 
