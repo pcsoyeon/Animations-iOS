53f91e0f5e34c3745dd92d246cde92841d5268a0
pcsoyeon <alice9809@naver.com>
2021-06-17 00:20:01 +0900

    [Feat] close animation 구현

--- LockScreenAnimation/LockScreenAnimation/ViewController.swift
+++ LockScreenAnimation/LockScreenAnimation/ViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // MARK: - Properties
+    // MARK: - UIComponents
     
     @IBOutlet weak var topLock: UIImageView!
     @IBOutlet weak var bottomLock: UIImageView!
     @IBOutlet weak var lockBorder: UIImageView!
     @IBOutlet weak var lockKeyhole: UIImageView!
-    
+
     // MARK: - LifeCycle Methods
     
     override func viewDidLoad() {
@@ -25,9 +25,9 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Custom Methods
+    
     func openLock() {
         UIView.animate(withDuration: 0.4, delay: 3.0, options: [], animations: {
-            
             // Rotate keyhole.
             self.lockKeyhole.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
             
@@ -35,19 +35,39 @@ class ViewController: UIViewController {
                 UIView.animate(withDuration: 0.5, delay: 0.2, options: [], animations: {
                     
                     // Open lock.
-                    let yDelta = self.lockBorder.frame.maxY
+                    let yDelta = CGFloat(210)
                     
                     self.topLock.center.y -= yDelta
                     self.lockKeyhole.center.y -= yDelta
                     self.lockBorder.center.y -= yDelta
                     self.bottomLock.center.y += yDelta
                     }, completion: { _ in
-                        self.topLock.removeFromSuperview()
-                        self.lockKeyhole.removeFromSuperview()
-                        self.lockBorder.removeFromSuperview()
-                        self.bottomLock.removeFromSuperview()
+//                        self.topLock.removeFromSuperview()
+//                        self.lockKeyhole.removeFromSuperview()
+//                        self.lockBorder.removeFromSuperview()
+//                        self.bottomLock.removeFromSuperview()
                 })
         })
+    }
+    
+    // MARK: - Action Methods
+    
+    @IBAction func touchUpCloseButton(_ sender: Any) {
+        UIView.animate(withDuration: 0.4, animations: {
+            // Close lock.
+            let yDelta = CGFloat(210)
+            
+            self.topLock.center.y += yDelta
+            self.lockKeyhole.center.y += yDelta
+            self.lockBorder.center.y += yDelta
+            self.bottomLock.center.y -= yDelta
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.5, animations: {
+                // Rotate keyhole.
+                self.lockKeyhole.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi))
+            })
+        })
+        
     }
     
     // MARK: - Status Bar
