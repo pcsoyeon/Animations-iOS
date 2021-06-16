e0fb6f555fb51ee33c775187b50dcc0d8dec7098
pcsoyeon <alice9809@naver.com>
2021-06-16 23:01:26 +0900

    [Feat] lock animation 구현

--- LockScreenAnimation/LockScreenAnimation/ViewController.swift
+++ LockScreenAnimation/LockScreenAnimation/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    @IBOutlet weak var topLock: UIImageView!
+    @IBOutlet weak var bottomLock: UIImageView!
+    @IBOutlet weak var lockBorder: UIImageView!
+    @IBOutlet weak var lockKeyhole: UIImageView!
+    
+    // MARK: - LifeCycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        openLock()
     }
-
+    
+    // MARK: - Custom Methods
+    
+    func openLock() {
+        UIView.animate(withDuration: 0.4, delay: 5.0, options: [], animations: {
+            
+            // Rotate keyhole.
+            self.lockKeyhole.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
+            
+            }, completion: { _ in
+                UIView.animate(withDuration: 0.5, delay: 0.2, options: [], animations: {
+                    
+                    // Open lock.
+                    let yDelta = self.lockBorder.frame.maxY
+                    
+                    self.topLock.center.y -= yDelta
+                    self.lockKeyhole.center.y -= yDelta
+                    self.lockBorder.center.y -= yDelta
+                    self.bottomLock.center.y += yDelta
+                    }, completion: { _ in
+                        self.topLock.removeFromSuperview()
+                        self.lockKeyhole.removeFromSuperview()
+                        self.lockBorder.removeFromSuperview()
+                        self.bottomLock.removeFromSuperview()
+                })
+        })
+    }
+    
+    // MARK: - Status Bar
+    
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
 
 }
 
